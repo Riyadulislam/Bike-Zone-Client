be03b6a1b7500f9249cb661126f6e359e47c5fde
Riyadulislam <riyadulislambd02@gmail.com>
2023-01-04 01:46:30 +0600

    pic change

--- src/Pages/Home/Category/Categorycard/Categorycard.js
+++ src/Pages/Home/Category/Categorycard/Categorycard.js
@@ -14,7 +14,7 @@ const Categorycard = ({category,isLoading}) => {
     return (
         <div>
         
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
     <img src={image} alt="Shoes" className="rounded-xl" />
   </figure>
